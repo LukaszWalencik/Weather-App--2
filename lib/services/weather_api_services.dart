ef578a1e97d762531bdb57c86915606553ad1c54
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-08-30 22:10:48 +0200

    Add getWeather function which use lat and lon (get from getDirectGeocoding function to get information about weather correct city

--- lib/services/weather_api_services.dart
+++ lib/services/weather_api_services.dart
@@ -6,6 +6,7 @@ import 'package:http/http.dart' as http;
 import 'package:weather_app2/constants/constants.dart';
 import 'package:weather_app2/exceptions/weather_exception.dart';
 import 'package:weather_app2/models/direct_geocoding.dart';
+import 'package:weather_app2/models/weather.dart';
 import 'package:weather_app2/services/http_error_handler.dart';
 
 class WeatherApiServices {
@@ -35,6 +36,30 @@ class WeatherApiServices {
       }
       final directGeocoding = DirectGeocoding.fromJson(responseBody);
       return directGeocoding;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  Future<Weather> getWeather(DirectGeocoding directGeocoding) async {
+    final Uri uri = Uri(
+        scheme: 'https',
+        host: kApiHost,
+        path: '/data/2.5/weather',
+        queryParameters: {
+          'lat': '${directGeocoding.lat}',
+          'lon': '${directGeocoding.lon}',
+          'units': kUnit,
+          'appid': dotenv.env['APPID']
+        });
+    try {
+      http.Response response = await httpClient.get(uri);
+      if (response.statusCode != 200) {
+        throw Exception(httpErrorHandler(response));
+      }
+      final weatherJson = jsonDecode(response.body);
+      final Weather weather = Weather.fromJson(weatherJson);
+      return weather;
     } catch (e) {
       rethrow;
     }
