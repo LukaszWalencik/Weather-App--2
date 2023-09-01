6602c02699d9127ea7891ab7e00dbec338cab919
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-01 15:10:18 +0200

    Create weather repository

---
+++ lib/repositories/weather_repository.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:weather_app2/exceptions/weather_exception.dart';
+import 'package:weather_app2/models/custom_error.dart';
+import 'package:weather_app2/models/direct_geocoding.dart';
+import 'package:weather_app2/models/weather.dart';
+import 'package:weather_app2/services/weather_api_services.dart';
+
+class WeatherRepository {
+  final WeatherApiServices weatherApiServices;
+  WeatherRepository({
+    required this.weatherApiServices,
+  });
+
+  Future<Weather> fetchDirectGeocoding(String city) async {
+    try {
+      final DirectGeocoding directGeocoding =
+          await weatherApiServices.getDirectGeocoding(city);
+      print('DirectGeocoding: $directGeocoding');
+      final Weather tempWeather =
+          await weatherApiServices.getWeather(directGeocoding);
+      final Weather weather = tempWeather.copyWith(
+          name: directGeocoding.name, country: directGeocoding.country);
+      print('Weather: $weather');
+
+      return weather;
+    } on WeatherException catch (e) {
+      throw CustomError(errorMessage: e.message!);
+    } catch (e) {
+      throw CustomError(errorMessage: e.toString());
+    }
+  }
+}
