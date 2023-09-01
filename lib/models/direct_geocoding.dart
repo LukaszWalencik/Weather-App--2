64486f7db0eac6cce2824bb5f673cc65c6b922d8
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-01 15:49:33 +0200

    Fix DirectGeocoding model

--- lib/models/direct_geocoding.dart
+++ lib/models/direct_geocoding.dart
@@ -4,26 +4,21 @@ import 'dart:convert';
 import 'package:equatable/equatable.dart';
 
 class DirectGeocoding extends Equatable {
-  final String name;
-  final String lat;
-  final String lon;
-  final String country;
+  final double lat;
+  final double lon;
   DirectGeocoding({
-    required this.name,
     required this.lat,
     required this.lon,
-    required this.country,
   });
 
   @override
-  List<Object> get props => [name, lat, lon, country];
+  List<Object> get props => [lat, lon];
 
   factory DirectGeocoding.fromJson(List<dynamic> json) {
     final Map<String, dynamic> data = json[0];
     return DirectGeocoding(
-        name: data['name'],
-        lat: data['lat'],
-        lon: data['lon'],
-        country: data['country']);
+      lat: data['lat'],
+      lon: data['lon'],
+    );
   }
 }
