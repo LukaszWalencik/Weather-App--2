4211719098cff89976b4080de53c6a9d775c6fbf
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-01 20:08:31 +0200

    Create a simple cubit test using initState in HomePage

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app2/cubits/weather/weather_cubit.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -8,6 +10,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<WeatherCubit>().fetchWeather('London');
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
