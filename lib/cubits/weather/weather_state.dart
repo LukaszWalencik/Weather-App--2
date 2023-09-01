cddd044a57fd97c5030a920907d6ccd298905897
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-01 19:49:56 +0200

    Create WeatherStatus enum
    Edit Weather State, add variables, create state.initial, copyWith, toString, equatable

--- lib/cubits/weather/weather_state.dart
+++ lib/cubits/weather/weather_state.dart
@@ -1,10 +1,38 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'weather_cubit.dart';
 
-sealed class WeatherState extends Equatable {
-  const WeatherState();
+enum WeatherStatus { initial, loading, loaded, error }
+
+class WeatherState extends Equatable {
+  final Weather weather;
+  final CustomError error;
+  final WeatherStatus status;
+  WeatherState({
+    required this.weather,
+    required this.error,
+    required this.status,
+  });
+  factory WeatherState.initial() {
+    return WeatherState(
+        weather: Weather.initial(),
+        error: CustomError(),
+        status: WeatherStatus.initial);
+  }
+  @override
+  List<Object> get props => [weather, error, status];
 
   @override
-  List<Object> get props => [];
-}
+  bool get stringify => true;
 
-final class WeatherInitial extends WeatherState {}
+  WeatherState copyWith({
+    Weather? weather,
+    CustomError? error,
+    WeatherStatus? status,
+  }) {
+    return WeatherState(
+      weather: weather ?? this.weather,
+      error: error ?? this.error,
+      status: status ?? this.status,
+    );
+  }
+}
