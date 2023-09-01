f1f00c1d239143ff8055bf4c7794c88d36eed5be
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-01 20:08:00 +0200

    Add RepositoryProvider and BlocProvider to inject WeatherCubit to UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:weather_app2/cubits/weather/weather_cubit.dart';
 import 'package:weather_app2/pages/my_home_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app2/repositories/weather_repository.dart';
+import 'package:http/http.dart' as http;
+import 'package:weather_app2/services/weather_api_services.dart';
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -12,14 +17,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Weather App',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return RepositoryProvider(
+      create: (context) => WeatherRepository(
+          weatherApiServices: WeatherApiServices(httpClient: http.Client())),
+      child: BlocProvider(
+        create: (context) =>
+            WeatherCubit(weatherRepository: context.read<WeatherRepository>()),
+        child: MaterialApp(
+          title: 'Weather App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const MyHomePage(),
+        ),
       ),
-      home: const MyHomePage(),
     );
   }
 }
