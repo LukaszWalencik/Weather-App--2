d53432ffa473a78af11f09ca0db03f387cfd9baf
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-03 20:35:33 +0200

    Copy state from WeatherCubit

--- lib/blocs/weather/weather_state.dart
+++ lib/blocs/weather/weather_state.dart
@@ -1,10 +1,38 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'weather_bloc.dart';
 
-sealed class WeatherState extends Equatable {
-  const WeatherState();
-  
-  @override
-  List<Object> get props => [];
-}
+enum WeatherStatus { initial, loading, loaded, error }
 
-final class WeatherInitial extends WeatherState {}
+class WeatherState extends Equatable {
+  final Weather weather;
+  final CustomError error;
+  final WeatherStatus status;
+  WeatherState({
+    required this.weather,
+    required this.error,
+    required this.status,
+  });
+  factory WeatherState.initial() {
+    return WeatherState(
+        weather: Weather.initial(),
+        error: CustomError(),
+        status: WeatherStatus.initial);
+  }
+  @override
+  List<Object> get props => [weather, error, status];
+
+  @override
+  bool get stringify => true;
+
+  WeatherState copyWith({
+    Weather? weather,
+    CustomError? error,
+    WeatherStatus? status,
+  }) {
+    return WeatherState(
+      weather: weather ?? this.weather,
+      error: error ?? this.error,
+      status: status ?? this.status,
+    );
+  }
+}
