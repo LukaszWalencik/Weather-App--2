779a0cef12b048e44063df570b39910a046dacc1
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-03 15:16:29 +0200

    Edit Cubit and add toggleTempUnit to change unit

--- lib/cubits/temp_settings/temp_settings_cubit.dart
+++ lib/cubits/temp_settings/temp_settings_cubit.dart
@@ -4,5 +4,13 @@ import 'package:equatable/equatable.dart';
 part 'temp_settings_state.dart';
 
 class TempSettingsCubit extends Cubit<TempSettingsState> {
-  TempSettingsCubit() : super(TempSettingsInitial());
+  TempSettingsCubit() : super(TempSettingsState.initial());
+
+  void toggleTempUnit() {
+    emit(state.copyWith(
+        tempUnit: state.tempUnit == TempUnit.celsius
+            ? TempUnit.fahrenheit
+            : TempUnit.celsius));
+    print('tempUnit: $state');
+  }
 }
