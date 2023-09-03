02bd0a5458cef4646061aa529f629e4316d48121
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-03 15:10:35 +0200

    Add temp settings cubit

---
+++ lib/cubits/temp_settings/temp_settings_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'temp_settings_state.dart';
+
+class TempSettingsCubit extends Cubit<TempSettingsState> {
+  TempSettingsCubit() : super(TempSettingsInitial());
+}
