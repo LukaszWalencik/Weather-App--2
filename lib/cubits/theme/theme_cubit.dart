d657ea61dcb1a30b6b171acd7b7bfd33c9c90059
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-03 19:56:54 +0200

    Edit ThemeCubit Add toggleTheme method to change theme between light and dark

--- lib/cubits/theme/theme_cubit.dart
+++ lib/cubits/theme/theme_cubit.dart
@@ -4,5 +4,12 @@ import 'package:equatable/equatable.dart';
 part 'theme_state.dart';
 
 class ThemeCubit extends Cubit<ThemeState> {
-  ThemeCubit() : super(ThemeInitial());
+  ThemeCubit() : super(ThemeState.initial());
+
+  void toggleTheme() {
+    emit(state.copyWith(
+        appTheme:
+            state.appTheme == AppTheme.light ? AppTheme.dark : AppTheme.light));
+    print('Theme state $state');
+  }
 }
