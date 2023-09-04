3945d4885d5dfc37408e346df614764fa711822c
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-09-04 21:53:04 +0200

    Create changeTheme

--- lib/blocs/theme/theme_bloc.dart
+++ lib/blocs/theme/theme_bloc.dart
@@ -5,9 +5,12 @@ part 'theme_event.dart';
 part 'theme_state.dart';
 
 class ThemeBloc extends Bloc<ThemeEvent, ThemeState> {
-  ThemeBloc() : super(ThemeInitial()) {
-    on<ThemeEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  ThemeBloc() : super(ThemeState.initial()) {
+    on<ChangeThemeEvent>(_changeTheme);
+  }
+  void _changeTheme(ChangeThemeEvent event, Emitter emit) {
+    emit(state.copyWith(
+        appTheme:
+            state.appTheme == AppTheme.light ? AppTheme.dark : AppTheme.light));
   }
 }
